5404f882dfcfdb0ec2d64b0021bb9ccb273464e8
Julián García <julian2996@gmail.com>
2018-03-10 14:18:29 -0600

    Update customink.py

--- customink.py
+++ customink.py
@@ -8,18 +8,23 @@ downloader = Downloader()
 
 all_subcategories = []
 counter = 0
-for category, name in categories:
+for category, name in categories[2:]:
     subcategories_json = requests.get('https://www.customink.com/clipart/clipart_categories/{}.json?all=true'.format(category)).json()
     subcategories = [subcategory['id'] for subcategory in subcategories_json['_embedded']['children']]
     # all_subcategories.extend(subcategories)
     for sindex, subcategory in enumerate(subcategories):
-        items_json = requests.get('https://www.customink.com/clipart/browse/{}.json'.format(subcategory)).json()
-        items = [item['id'] for item in items_json['_embedded']['cliparts']]
-        for index, item in enumerate(items):
-            print '{0}/{1} C, {2}/{3} SC, {4}/{5} I'.format(counter, len(categories), sindex, len(subcategories), index, len(items)),
-            url = 'https://clipart-manipulate.out.customink.com/prod/manipulate?height=2500&width=2500&red=0&green=0&blue=0&clipart_id={}&path=%2Fclipart%2Feps%2F&fV=false&fH=false&lockRatio=true&transparent=true&grayscale=false&blackwhite=false&rotate=0'.format(item)
-            
-            downloader.download_file_category(url, name, item)
+        try:
+            items_json = requests.get('https://www.customink.com/clipart/browse/{}.json'.format(subcategory)).json()
+            items = [item['id'] for item in items_json['_embedded']['cliparts']]
+            for index, item in enumerate(items):
+                try:
+                    print '{0}/{1} C, {2}/{3} SC, {4}/{5} I'.format(counter, len(categories), sindex, len(subcategories), index, len(items)),
+                    url = 'https://clipart-manipulate.out.customink.com/prod/manipulate?height=2500&width=2500&red=0&green=0&blue=0&clipart_id={}&path=%2Fclipart%2Feps%2F&fV=false&fH=false&lockRatio=true&transparent=true&grayscale=false&blackwhite=false&rotate=0'.format(item)
+                    downloader.download_file_category(url, name, item)
+                except:
+                    continue
+        except:
+            continue
         # all_items.extend(items)
     counter += 1
 
