09122c2de21213cdb1418909949c1164405e2b12
Julián García <julian2996@gmail.com>
2018-03-10 02:41:55 -0600

    Update customink.py

--- customink.py
+++ customink.py
@@ -16,7 +16,7 @@ for category, name in categories:
         items_json = requests.get('https://www.customink.com/clipart/browse/{}.json'.format(subcategory)).json()
         items = [item['id'] for item in items_json['_embedded']['cliparts']]
         for index, item in enumerate(items):
-            print '{0}/{1} C, {2}/{3} SC, {4}/{5} I'.format(counter, len(categories), sindex, len(subcategories), index, len(items),
+            print '{0}/{1} C, {2}/{3} SC, {4}/{5} I'.format(counter, len(categories), sindex, len(subcategories), index, len(items)),
             url = 'https://clipart-manipulate.out.customink.com/prod/manipulate?height=2500&width=2500&red=0&green=0&blue=0&clipart_id={}&path=%2Fclipart%2Feps%2F&fV=false&fH=false&lockRatio=true&transparent=true&grayscale=false&blackwhite=false&rotate=0'.format(item)
             
             downloader.download_file_category(url, name, item)
