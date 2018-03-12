a87d8b95d6de8b9f744800e5def45a5488c57301
Julián García <julian2996@gmail.com>
2018-03-11 23:46:30 -0600

    Update customink.py

--- customink.py
+++ customink.py
@@ -1,5 +1,6 @@
 import requests
 from files.downloader import Downloader
+import sys
 
 categories_json = requests.get('https://www.customink.com/clipart/clipart_categories.json').json()
 categories = [(category['id'], '-'.join(category['name'].split('/'))) for category in categories_json[u'_embedded'][u'clipart_categories']]
@@ -19,7 +20,11 @@ for category, name in categories:
             for index, item in enumerate(items):
                 try:
                     print '{0}/{1} C, {2}/{3} SC, {4}/{5} I'.format(counter, len(categories), sindex, len(subcategories), index, len(items)),
-                    url = 'https://clipart-manipulate.out.customink.com/prod/manipulate?height=2500&width=2500&red=0&green=0&blue=0&clipart_id={}&path=%2Fclipart%2Feps%2F&fV=false&fH=false&lockRatio=true&transparent=true&grayscale=false&blackwhite=false&rotate=0'.format(item)
+                    try:
+                        res = sys.argv[1]
+                    except:
+                        res = '2500'
+                    url = 'https://clipart-manipulate.out.customink.com/prod/manipulate?height={0}&width={1}&red=0&green=0&blue=0&clipart_id={2}&path=%2Fclipart%2Feps%2F&fV=false&fH=false&lockRatio=true&transparent=true&grayscale=false&blackwhite=false&rotate=0'.format(res, res, item)
                     downloader.download_file_category(url, name, item)
                 except:
                     print 'Skipped item'
@@ -30,4 +35,4 @@ for category, name in categories:
         # all_items.extend(items)
     counter += 1
 
-print 'h'
+print 'Done.'
